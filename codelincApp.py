064bbe15502a36d7a20c4d535ec495a814e265d9
Lenny Uwaeme <48912140+lennythecreator@users.noreply.github.com>
2023-04-15 18:37:57 -0400

    new change

--- codelincApp.py
+++ codelincApp.py
@@ -1,9 +1,19 @@
-from flask import Flask 
+from flask import Flask, render_template, request
 
 app= Flask(__name__)
-@app.route("/")
-def hello():
-    return 'Hello World'
+
+
+@app.route('/')
+@app.route('/hello')
+@app.route('/hello/<user>')
+def hello_world(user=None):
+    user = user or 'Shalabh'
+    return render_template('index.html', user=user)
+
+@app.route('/profile/<profile>')
+def profile(profile=None):
+    profile = profile or 'Shalabh'
+    return render_template('profile.html', profile=profile)
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
